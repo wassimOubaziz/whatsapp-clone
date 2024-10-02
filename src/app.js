cd6ecffdbe7e150b1abf5fe9bfb5355882e54dba
Wassim Oubaziz <wassimdjaafar5@gmail.com>
2024-10-02 12:10:51 +0100

    Update environment variables and package dependencies

--- src/app.js
+++ src/app.js
@@ -1,6 +1,49 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import mongoSanitize from "express-mongo-sanitize";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import fileUpload from "express-fileupload";
+import cors from "cors";
 
 //create express app
 const app = express();
+
+//morgan
+if (process.env.NODE_ENV !== "production") {
+  app.use(morgan("dev"));
+}
+
+//helmet (security)
+app.use(helmet());
+
+//parse json request body
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//sanitize request data
+app.use(mongoSanitize());
+
+//enable cookies parser
+app.use(cookieParser());
+
+//gzip compression
+app.use(compression());
+
+//file upload
+app.use(fileUpload({ useTempFiles: true }));
+
+//cors
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
+
+app.get("/test", (req, res) => {
+  res.json({ message: "test route is working" });
+});
 
 export default app;
